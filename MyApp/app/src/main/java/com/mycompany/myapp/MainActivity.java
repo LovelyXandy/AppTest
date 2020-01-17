29f387a7837c3acae4fff915084f33aebecd5044
LovelyXandy <drajwilliamson@gmail.com>
2020-01-17 15:00:54 +0000

    Attempting to merge simple app to test git

---
+++ MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp;
+
+import android.app.*;
+import android.widget.*;
+import android.view.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+	AlertDialog.Builder builder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		builder = new AlertDialog.Builder(this);  
+		
+		final Button button = findViewById(R.id.button1);
+		final EditText firstName = findViewById(R.id.editTextName);
+		final EditText lastName = findViewById(R.id.editTextLastName);
+		button.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					builder.setMessage(getResources().getString(R.string.hi) + firstName.getText().toString() + " " + lastName.getText());
+					AlertDialog alert = builder.create();  
+					//Setting the title manually  
+					alert.setTitle("AlertDialogExample");  
+					alert.show();  
+				}
+			});
+    }
+}
