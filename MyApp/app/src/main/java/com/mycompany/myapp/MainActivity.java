0c7b5b4cedb8cfddbe10563a232404cd6c6e20be
LovelyXandy <drajwilliamson@gmail.com>
2020-01-17 18:03:32 +0000

    En GB Stribg used for Locale

--- MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends Activity
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
 						RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-GB");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
 						"Hi say something");
         try {
