9ea9c71b8496f2bbe67c17d85e0a30bf0b9adb22
LovelyXandy <Drajwilliamson@gmail.com>
2020-01-17 17:38:40 +0000

    listening attemot

--- MyApp/app/build/gen/com/mycompany/myapp/R.java
+++ MyApp/app/build/gen/com/mycompany/myapp/R.java
@@ -15,9 +15,10 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f060000;
-        public static final int editTextLastName=0x7f060003;
-        public static final int editTextName=0x7f060002;
-        public static final int textView1=0x7f060001;
+        public static final int button2=0x7f060001;
+        public static final int editTextLastName=0x7f060004;
+        public static final int editTextName=0x7f060003;
+        public static final int textView1=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -6,10 +6,19 @@ import android.view.*;
 import android.os.*;
 import android.speech.tts.*;
 import java.util.Locale;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity 
 {
+	TextView firstName;
+	TextView lastName;
 	AlertDialog.Builder builder;
+	private final int REQ_CODE_SPEECH_INPUT = 100;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,8 +28,16 @@ public class MainActivity extends Activity
 		final TextToSpeech talker = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {public void onInit(int status) {}
 		});
 		final Button button = findViewById(R.id.button1);
-		final EditText firstName = findViewById(R.id.editTextName);
-		final EditText lastName = findViewById(R.id.editTextLastName);
+		final Button button2 = findViewById(R.id.button2);
+		firstName = findViewById(R.id.editTextName);
+		lastName = findViewById(R.id.editTextLastName);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					askSpeechInput();
+				}
+			});
 		button.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
 					builder.setMessage(getResources().getString(R.string.hi) + firstName.getText().toString() + " " + lastName.getText());
@@ -33,4 +50,41 @@ public class MainActivity extends Activity
 				}
 			});
     }
+	
+	// Showing google speech input dialog
+
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+						RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+						"Hi say something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    // Receiving speech input
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+					if (resultCode == RESULT_OK && null != data) {
+
+						ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+						firstName.setText(result.get(0));
+					}
+					break;
+				}
+
+        }
+		
+	}
 }
